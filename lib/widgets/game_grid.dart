78c56f208a70c21aaa481ad32109ce13e49cdd57
jheel0802 <jheel0802@gmail.com>
2022-11-20 14:44:56 +0530

    wordsadded

--- lib/widgets/game_grid.dart
+++ lib/widgets/game_grid.dart
@@ -1,33 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lava_flutter/providers/game_settings_provider.dart';
 import 'package:lava_flutter/widgets/game_letterbox.dart';
 import 'package:lava_flutter/widgets/game_row.dart';
 
 const rows = 5;
 const wordsize = 5;
 
-class GameGrid extends StatelessWidget {
+class GameGrid extends ConsumerWidget {
   const GameGrid({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final gameSettings = ref.watch(gameSettingsProvider);
+    final wordSize = gameSettings.wordsize;
     return Container(
-      child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-        GameRow(
-          wordsize: 5,
-        ),
-        GameRow(
-          wordsize: 5,
-        ),
-        GameRow(
-          wordsize: 5,
-        ),
-        GameRow(
-          wordsize: 5,
-        ),
-        GameRow(
-          wordsize: 5,
-        ),
-      ]),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          GameRow(
+            wordsize: wordSize,
+          ),
+          GameRow(
+            wordsize: wordSize,
+          ),
+          GameRow(
+            wordsize: wordSize,
+          ),
+          GameRow(
+            wordsize: wordSize,
+          ),
+          GameRow(
+            wordsize: wordSize,
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/widgets/lava_title_bar.dart
+++ lib/widgets/lava_title_bar.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lava_flutter/providers/game_settings_provider.dart';
 
 class LavaTitleBar extends StatelessWidget {
-  const LavaTitleBar({super.key});
+  const LavaTitleBar({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -10,20 +12,41 @@ class LavaTitleBar extends StatelessWidget {
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Container(
-            child: Text("high"),
-            margin: EdgeInsets.fromLTRB(0, 0, 30, 0),
-          ),
           Text(
             "LAVA",
             style: TextStyle(fontSize: 48),
           ),
-          Container(
-            child: Text("4"),
-            margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
-          )
+          WordSizeToggle()
         ],
       ),
+    );
+  }
+}
+
+class WordSizeToggle extends ConsumerWidget {
+  const WordSizeToggle({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final gameSettings = ref.watch(gameSettingsProvider);
+    final gameSettingsNotifier = ref.read(gameSettingsProvider.notifier);
+
+    void _updateWordSize() {
+      var newWordSize = 5;
+      if (gameSettings.wordsize == 4) newWordSize = 5;
+      if (gameSettings.wordsize == 5) newWordSize = 6;
+      if (gameSettings.wordsize == 6) newWordSize = 4;
+      gameSettingsNotifier.updateWordsize(newWordSize);
+    }
+
+    return Container(
+      child: OutlinedButton(
+        child: Text(gameSettings.wordsize.toString()),
+        onPressed: _updateWordSize,
+      ),
+      margin: EdgeInsets.fromLTRB(30, 0, 0, 0),
     );
   }
 }
