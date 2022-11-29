2d0eceb9efd29a3a85b7549fb879c745f0230821
chaitanyapatil21 <chaitanya@godamwale.com>
2022-11-29 21:37:43 +0530

    Revert "minor changes"
    This reverts commit 1ef999cd3d99f97be0e742e8bf85fdc756822f96.

--- lib/providers/game_state_provider.dart
+++ lib/providers/game_state_provider.dart
@@ -1,4 +1,5 @@
 import 'dart:math';
+
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:lava_flutter/data/word_repo.dart';
 import 'package:lava_flutter/providers/game_settings_provider.dart';
@@ -10,31 +11,26 @@ class GameState {
   final List<String> attempts;
   final int attempted;
 
-  const GameState({
-    required this.validWords,
+  const GameState(
+    {required this.validWords,
     required this.correctWord,
     required this.settings,
     required this.attempts,
     required this.attempted,
-  });
+    });
 
-  GameState clone(
-      {List<String>? validWords,
-      String? correctWord,
-      List<String>? attempts,
-      int? attempted}) {
+  GameState clone({List<String>? validWords, String? correctWord, List<String>? attempts, int? attempted}) {
     return GameState(
-        validWords: validWords ?? this.validWords,
-        correctWord: correctWord ?? this.correctWord,
-        settings: this.settings,
-        attempted: attempted ?? this.attempted,
-        attempts: attempts ?? this.attempts);
+      validWords: validWords ?? this.validWords,
+      correctWord: this.correctWord,
+      settings: this.settings,
+      attempted: attempted ?? this.attempted,
+      attempts: attempts ?? this.attempts);
   }
 }
 
 class GameStateNotifier extends StateNotifier<GameState> {
   final Random rng = Random();
-
   GameStateNotifier(GameSettings settings)
       : super(GameState(
             validWords: [],
@@ -55,51 +51,50 @@ class GameStateNotifier extends StateNotifier<GameState> {
             state.validWords[rng.nextInt(state.validWords.length - 1)]);
   }
 
-  void updateCurrentAttempt(String key) {
+  void updateCurrentAttempt(String key){
     final attempts = state.attempts;
-    if (attempts.length <= state.attempted) {
-      attempts.add("");
-    }
-    var currentAttempt = attempts[state.attempted];
+      if (attempts.length <= state.attempted){
+        attempts.add("");
+      }
+      var currentAttempt = attempts[state.attempted];
+    if(key == "_"){
 
-    if (key == "_") {
-      // handle enter press
-
-      if (currentAttempt.length < state.settings.wordsize) {
+      if(currentAttempt.length<state.settings.wordsize){
         print("attempted word incomplete");
         return;
       }
 
-      if (!state.validWords.contains(currentAttempt)) {
-        print("not in valid words list");
-        return;
-      }
+      state=state.clone(
+        attempted: state.attempted+1
+      );
 
-      state = state.clone(attempted: state.attempted + 1);
-    } else if (key == "<") {
-      // handle backpress
-      if (currentAttempt.isEmpty) {
-        print("cannot backspace on empty string");
+    } else if (key == "<"){
+      if(currentAttempt.isEmpty){
+        print("cannot backsapce on empty string");
         return;
       }
-      currentAttempt = currentAttempt.substring(0, currentAttempt.length - 1);
+      currentAttempt = currentAttempt.substring(0, currentAttempt.length -1);
       attempts[state.attempted] = currentAttempt;
-      state = state.clone(attempts: attempts);
-    } else {
-      if (currentAttempt.length >= state.settings.wordsize) {
+      state = state.clone(
+        attempts: attempts
+      );
+    } else{
+      if (currentAttempt.length >= state.settings.wordsize){
         print("trying to type word longer than correct word length");
         return;
       }
       currentAttempt += key;
-
+      print(currentAttempt);
       attempts[state.attempted] = currentAttempt;
-      state = state.clone(attempts: attempts);
+      state = state.clone(
+        attempts: attempts
+      );
     }
   }
+
 }
 
-final gameStateProvider =
-    StateNotifierProvider<GameStateNotifier, GameState>((ref) {
+final gameStateProvider = StateNotifierProvider<GameStateNotifier, GameState>((ref) {
   final settings = ref.watch(gameSettingsProvider);
   final gameStateNotifier = GameStateNotifier(settings);
   gameStateNotifier.updateWords();
